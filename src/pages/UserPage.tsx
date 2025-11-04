54165c4695f47727d4bc33565078aa840b8f0777
K0STI@ <ksinogub333@gmail.com>
2025-11-04 21:49:19 +0100

    little commit

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -58,7 +58,7 @@ export default function UserPage(): JSX.Element {
           </div>
 
           <div className=" absolute flex gap-5 bottom-3 gh ">
-            <a href="https://github.com/KOSTIAAANTYN/financeapp/tree/JWTv2" target='blanc'><img title='Frontend Part' className=' w-10 h-auto' src={gh} alt="gh-front" /></a>
+            <a href="https://github.com/KOSTIAAANTYN/financeapp-front-end" target='blanc'><img title='Frontend Part' className=' w-10 h-auto' src={gh} alt="gh-front" /></a>
             <a href="https://github.com/KOSTIAAANTYN/financeapp/tree/JWTv2" target='blanc'><img title='Backend Part' className=' w-10 h-auto' src={gh} alt="gh-back" /></a>
           </div>
         </div>
