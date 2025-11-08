4e5d9346615c3b59241584fe46080f0e7a86885c
K0STI@ <ksinogub333@gmail.com>
2025-11-08 15:18:18 +0100

    tst4

--- craco.config.js
+++ craco.config.js
@@ -2,9 +2,8 @@ const path = require('path');
 module.exports = {
   webpack: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@slices': path.resolve(__dirname, 'src/store/slices'),
-      '@imgs': path.resolve(__dirname, 'src/assets/imgs'),
-    },
-  },
+      '@slices': path.resolve(__dirname, './src/store/slices'),
+      '@imgs': path.resolve(__dirname, './src/assets/imgs')
+    }
+  }
 };
